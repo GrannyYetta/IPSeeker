f841dea10ab74c57463995244252f21f5c07eea2
GrannyYetta <you@example.com>
2022-09-03 13:20:10 +0200

    level-1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Request from "./Components/Request";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>What's My IP?</h1>
-      </header>
-      <p>THIS IS WHERE I INSERT THE IP ADDRESS</p>
+      <h1>What's My IP?</h1>
       <Request />
     </div>
   );
--- src/Components/Request.js
+++ src/Components/Request.js
@@ -2,21 +2,27 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Request = () => {
-  const [request, setRequest] = useState([]);
-  
-  useEffect(() => {
-    axios.get(
-      `https://geo.ipify.org/api/v2/country?apiKey=at_QsfJmhZiKJmwXSEGep2avwHQZlHfg`
-    );
+  const [request, setRequest] = useState("");
+
+  const getWithAxios = () => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=at_QsfJmhZiKJmwXSEGep2avwHQZlHfg`
+      )
       .then((response) => {
         setRequest(response.data.ip);
-        console.log(response.data.ip)
+        console.log(response.data.ip);
       })
       .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    getWithAxios();
+  });
+
   return (
     <div>
-      <h3> Your IP Address is: </h3>
+      <h3>{request}</h3>
     </div>
   );
 };
